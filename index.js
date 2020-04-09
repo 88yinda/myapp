f8c1bec785c295aee828d1065148b1b949af561b
DaYin <yinda6938@gmail.com>
2020-04-09 23:19:02 +0000

    install require path

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/templates/mymap.html')))
-app.use('/css', express.static('css'))
-app.use('/js', express.static('js'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/templates/mymap.html')));
+app.use('/css', express.static('./css/'));
+app.use('/js', express.static('./js/'));
 
-app.listen(port, () => console.log('Example app listening on port'+port+'!'))
+app.listen(port, () => console.log('Example app listening on port '+port+'!'))
