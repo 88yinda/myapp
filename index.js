01c22e054a7820a67548ba98bcd2fefa684cbe49
DaYin <yinda6938@gmail.com>
2020-04-10 00:06:12 +0000

    update index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/templates/mymap.html')));
-app.use('/css', express.static('./css/'));
-app.use('/js', express.static('./js/'));
+app.use('/css', express.static(__dirname + '/css/'));
+app.use('/js', express.static(__dirname + '/js/'));
 
 app.listen(port, () => console.log('Example app listening on port '+port+'!'))
